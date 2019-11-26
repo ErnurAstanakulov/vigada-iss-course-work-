5b50cdfcd7fa19e2f8b2b56f7b058c33f4cb1891
Maxim Marchuk <maxim.marchuk@yandex.ru>
2019-11-26 17:37:02 +0300

    moved check internet method to a separate class
    Переместил функцию проверки интернета в отдельный класс

--- vigada (iss course work)/LoaderViewController.swift
+++ vigada (iss course work)/LoaderViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
-import SystemConfiguration
 
 class LoaderViewController: UIViewController {
-
+    // MARK: - Properties
     private let loaderView = UIElements().containerView
-
     private var isInternet = true
 
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
 
+        let networkManager = NetworkManager()
+        networkManager.delegate = self
+        // Проверяем есть интернет или нет.
+        networkManager.checkInternet()
+
+        setupLoaderScreen()
+
+        self.loaderView.vgdLoader(.start, durationIn: 1.6)
+        let time = 2.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + time) { [weak self] in
+            var newViewController: UIViewController
+            self?.loaderView.vgdLoader(.stop)
+            // Если интернета нет, то будем смотреть данные из базы
+            if self?.isInternet ?? false {
+                newViewController = TabBarController()
+            } else {
+                newViewController = NoInternetViewController()
+            }
+            newViewController.modalTransitionStyle = .crossDissolve
+            self?.present(newViewController, animated: true, completion: nil)
+        }
+    }
+
+    // MARK: - Set up
+    func setupLoaderScreen() {
         guard let gifImageView = UIImageView.fromGif(frame: .zero, resourceName: "vgdLoadGIF") else {
             return
         }
@@ -40,52 +64,18 @@ class LoaderViewController: UIViewController {
             loaderView.widthAnchor.constraint(equalToConstant: 60),
             loaderView.heightAnchor.constraint(equalToConstant: 60)
             ])
-
-        isInternet = isConnectedToNetwork()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.loaderView.vgdLoader(.start, durationIn: 1.6)
-        let time = 2.5
-        DispatchQueue.main.asyncAfter(deadline: .now() + time) { [weak self] in
-            var newViewController: UIViewController
-            self?.loaderView.vgdLoader(.stop)
-            if self?.isInternet ?? false {
-                newViewController = TabBarController()
-            } else {
-                newViewController = NoInternetViewController()
-            }
-            newViewController.modalTransitionStyle = .crossDissolve
-            self?.present(newViewController, animated: true, completion: nil)
+}
+
+// MARK: - Extensions
+extension LoaderViewController: CheckInternetDelegate {
+    func checkInternet(_ isInternet: Bool) {
+        self.isInternet = isInternet
+
+        // Если интернет есть, сделаем предзагрузку контента
+        if isInternet {
+            //TODO: Добавить вызов метода предзагрузки контента из сети
         }
     }
-
-    // Проверка наличия интернета. Усли сети нет, то будуте показан специальны экран, на котором пользователь будет извещен о проблеме и ему будет предложено посмотреть сохраненные в CoreData данные.
-    // Пока эта функция тут. Позже она переедет в сетевой сервис.
-    // А тут, в лоадере, будет проверка сети и предзагрузка данных из сетевого каталога для первого экрана.
-    func isConnectedToNetwork() -> Bool {
-        var zeroAddress = sockaddr_in()
-        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
-        zeroAddress.sin_family = sa_family_t(AF_INET)
-
-        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
-            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) { zeroSockAddress in
-                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
-            }
-        }
-
-        var flags = SCNetworkReachabilityFlags()
-
-        if let defaultRouteReachability = defaultRouteReachability {
-            if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
-                return false
-            }
-        }
-
-        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
-        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-        return (isReachable && !needsConnection)
-    }
-
 }
--- vigada (iss course work)/NetworkManager.swift
+++ vigada (iss course work)/NetworkManager.swift
@@ -7,6 +7,11 @@
 //
 
 import Foundation
+import SystemConfiguration
+
+protocol CheckInternetDelegate: class {
+    func checkInternet(_ isInternet: Bool)
+}
 
 enum NetworkResponse: String {
     case success
@@ -35,5 +40,37 @@ func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String> {
         return .failure(NetworkResponse.outdated.rawValue)
     default:
         return .failure(NetworkResponse.failed.rawValue)
+    }
+}
+
+class NetworkManager {
+    weak var delegate: CheckInternetDelegate?
+
+    func checkInternet() {
+        self.delegate?.checkInternet(isConnectedToNetwork())
+    }
+
+    func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) { zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+
+        var flags = SCNetworkReachabilityFlags()
+
+        if let defaultRouteReachability = defaultRouteReachability {
+            if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+                return false
+            }
+        }
+
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
     }
 }

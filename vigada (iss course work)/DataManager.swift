d97d5a55136608515f2a093ef09e881701a4d969
Maxim Marchuk <maxim.marchuk@yandex.ru>
2019-12-01 05:28:51 +0300

    add data manager and builder network call
    Добавление файлов дата менеджера и билдера сетевых запросов

---
+++ vigada (iss course work)/DataManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataManager.swift
+//  vigada (iss course work)
+//
+//  Created by Maxim Marchuk on 28.11.2019.
+//  Copyright © 2019 Maxim Marchuk. All rights reserved.
+//
+
+import Foundation
---
+++ vigada (iss course work)/requestBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  requestBuilder.swift
+//  vigada (iss course work)
+//
+//  Created by Maxim Marchuk on 24.11.2019.
+//  Copyright © 2019 Maxim Marchuk. All rights reserved.
+//
+
+import Foundation

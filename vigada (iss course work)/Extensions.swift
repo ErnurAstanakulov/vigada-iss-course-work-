648c67bd3117059704ccbe81374859a084a9a441
Maxim Marchuk <maxim.marchuk@yandex.ru>
2019-12-06 16:42:11 +0300

    add rotate uiview method
    Добавил функцию вращения view

--- vigada (iss course work)/Extensions.swift
+++ vigada (iss course work)/Extensions.swift
@@ -31,3 +31,12 @@ extension UITableView {
         }
     }
 }
+
+extension UIView {
+    func rotate(degrees: CGFloat) {
+        let degreesToRadians: (CGFloat) -> CGFloat = { (degrees: CGFloat) in
+            return degrees / 180.0 * CGFloat.pi
+        }
+        self.transform =  CGAffineTransform(rotationAngle: degreesToRadians(degrees))
+    }
+}

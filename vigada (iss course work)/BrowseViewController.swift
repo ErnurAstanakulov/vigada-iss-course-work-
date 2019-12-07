03388040ccc3924f34bdaded400181e06d0701d6
Maxim Marchuk <maxim.marchuk@yandex.ru>
2019-12-07 15:42:45 +0300

    add UITableView
    Добавил UITableView

--- vigada (iss course work)/BrowseViewController.swift
+++ vigada (iss course work)/BrowseViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class BrowseViewController: UIViewController {
+    // MARK: - Properties
+    private let tableView = UITableView(frame: .zero, style: .plain)
+    private let reuseId = "UITableViewCellreuseId"
     // MARK: - UIViewController lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +19,72 @@ class BrowseViewController: UIViewController {
         title = "Browse"
         view.backgroundColor = UIColor.VGDColor.white
 
+        setupTableView()
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+    }
+
+    // MARK: - Set up
+    private func setupTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(SearchResultTableViewCell.self, forCellReuseIdentifier: "SearchResultTableViewCell")
+        tableView.register(SearchRecentTableViewCell.self, forCellReuseIdentifier: "SearchRecentTableViewCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorColor = UIColor.VGDColor.clear
+        tableView.keyboardDismissMode = .onDrag
+
+        tableView.backgroundColor = .yellow
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -0),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -0)
+            ])
+    }
+
+}
+
+// MARK: - Extensions
+extension BrowseViewController: UITableViewDataSource, UITableViewDelegate {
+
+    // MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchRecentTableViewCell", for: indexPath) as? SearchRecentTableViewCell
+        cell?.settingView.settingLabel.text = "игры..."
+        cell?.selectionStyle = .none
+        if let cell = cell {
+            return cell
+        } else {
+            return defaultCell(indexPath: indexPath)
+        }
+    }
+
+    func defaultCell(indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseId, for: indexPath)
+        cell.textLabel?.text = "Broke 'Settings' table row"
+        return cell
+    }
+
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("нажал")
     }
 }

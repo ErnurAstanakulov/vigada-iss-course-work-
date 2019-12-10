326a99ef648aea1aa6e139a9cf68b728284415f5
Maxim Marchuk <maxim.marchuk@yandex.ru>
2019-12-10 15:55:37 +0300

    add quote about games
    Добавил известные цитаты об играх, для отображения на экране браузинга

---
+++ vigada (iss course work)/QuotesAboutVideoGames.swift
@@ -0,0 +1,25 @@
+//
+//  QuotesAboutVideoGames.swift
+//  vigada (iss course work)
+//
+//  Created by Maxim Marchuk on 10.12.2019.
+//  Copyright © 2019 Maxim Marchuk. All rights reserved.
+//
+
+import Foundation
+
+let quote1 = "“Always trust computer games.“ ― Ridley Pearson"
+let quote2 = "“Reality is broken. Game designers can fix it.“ ― Jane McGonigal"
+let quote3 = "“Secrets are like herpes: If you got 'em, might as well spread 'em around.“ ― Harkin"
+let quote4 = "“We all make choices, but in the end our choices make us.“ ― Andrew Ryan"
+let quote5 = "“Get over here!“ – Scorpion, Mortal Kombat"
+let quote6 = "“A hero need not speak. When he is gone, the world will speak for him.“ – Halo"
+let quote7 = "“Trust me.“ BT-7274, Titanfall 2"
+let quote8 = "“Nothing is true, everything is permitted.“ Ezio Auditore, Assassin’s Creed 2"
+let quote9 = "“War. War never changes.“ Narrator, Fallout 3"
+let quote10 = "“...” Gordon Freeman, Half-Life"
+let quote11 = "“I miss the internet“ Graffiti, Left 4 Dead"
+let quote12 = "“Finish him!“ Mortal Kombat"
+let quote13 = "“Она - со мной, углепластик. Так охладите трахание. Я рассматриваю ее пользу.“"
+
+let quotesAboutVideoGames = [quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8, quote9, quote10, quote11, quote12, quote13]

08990389b49d064ddbd7a52bf37acf1ad7ba43c8
Maxim Marchuk <maxim.marchuk@yandex.ru>
2019-12-11 16:57:17 +0300

    add quote
    Добавил цитату

--- vigada (iss course work)/QuotesAboutVideoGames.swift
+++ vigada (iss course work)/QuotesAboutVideoGames.swift
@@ -13,7 +13,7 @@ let quote2 = "“Reality is broken. Game designers can fix it.“ ― Jane McGon
 let quote3 = "“Secrets are like herpes: If you got 'em, might as well spread 'em around.“ ― Harkin"
 let quote4 = "“We all make choices, but in the end our choices make us.“ ― Andrew Ryan"
 let quote5 = "“Get over here!“ – Scorpion, Mortal Kombat"
-let quote6 = "“A hero need not speak. When he is gone, the world will speak for him.“ – Halo"
+let quote6 = "“A hero need not speak. When he is gone, the world will speak for him.“ Halo"
 let quote7 = "“Trust me.“ BT-7274, Titanfall 2"
 let quote8 = "“Nothing is true, everything is permitted.“ Ezio Auditore, Assassin’s Creed 2"
 let quote9 = "“War. War never changes.“ Narrator, Fallout 3"
@@ -21,5 +21,6 @@ let quote10 = "“...” Gordon Freeman, Half-Life"
 let quote11 = "“I miss the internet“ Graffiti, Left 4 Dead"
 let quote12 = "“Finish him!“ Mortal Kombat"
 let quote13 = "“Она - со мной, углепластик. Так охладите трахание. Я рассматриваю ее пользу.“"
+let quote14 = "“Video games are a waste of time for men with nothing else to do. Real brains don't do that.“ – Ray Bradbury"
 
 let quotesAboutVideoGames = [quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8, quote9, quote10, quote11, quote12, quote13]
